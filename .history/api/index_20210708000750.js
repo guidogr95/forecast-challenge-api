099f04d18436c4c124273262c24d4175b60f9e04
guidogr95 <guido.gr@hotmail.com>
2021-07-08 00:21:42 -0500

    first

--- .history/api/index_20210708000750.js
+++ .history/api/index_20210708000750.js
@@ -16,6 +16,36 @@ app.use((req, res, next) => {
     next()
 });
 
+app.post('/api/getAirports', async (req, res) => {
+  const term = req.body.term
+  let params = {}
+  if (req.body.params) {
+      params = {
+          ...req.body.params
+      }
+  }
+  const headers = {
+    headers: {
+      'Content-Type': 'aplication/json',
+      'Accept': 'application/json',
+      'APC-Auth': '3cf5d1fd31',
+      'APC-Auth-Secret': '90df6df6e13e91c'
+    }
+  }
+  const apiUrl = 'https://www.air-port-codes.com/api/v1'
+
+  if (!term) {
+      return res.status(400).send("No term") 
+  } else {
+      try {
+          const data = await axios.post(`${apiUrl}/autocomplete?term=${term}`, {...params}, headers)
+          return res.status(200).json([...data.data.airports])
+      } catch (err) {
+          return res.status(500).json({ error: err.code })
+      }
+  }
+})
+
 app.get('/api/getCities/:id', async (req, res) => {
   const term = req.params.id
   const apiUrl = 'https://www.metaweather.com/api'
